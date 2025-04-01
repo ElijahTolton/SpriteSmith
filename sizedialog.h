b0303752827cd191e9c018b658c5c2a48b80356f
lancasteralex <lancasteralex25@gmail.com>
2025-03-31 19:45:37 -0600

    Template header file doc

--- sizedialog.h
+++ sizedialog.h
@@ -3,26 +3,54 @@
 
 #include <QDialog>
 
-namespace Ui {
-class SizeDialog;
-}
+namespace Ui { class SizeDialog; }
 
+/**
+ * @brief The initial size dialog window which sets the canvas size in pixels
+ *
+ * @author Alex Lancaster
+ * @date March 29, 2025
+ */
 class SizeDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief SizeDialog
+     * @param parent
+     */
     explicit SizeDialog(QWidget *parent = nullptr);
+
+    /**
+     *
+     */
     ~SizeDialog();
 
 public slots:
+
+    /**
+     * @brief emitSetSize
+     */
     void emitSetSize();
 
 signals:
+    /**
+     * @brief setSize
+     * @param canvasSize
+     */
     void setSize(int canvasSize);
 
 private:
+    /**
+     * @brief ui
+     */
     Ui::SizeDialog *ui;
+
+    /**
+     * @brief closeEvent
+     * @param event
+     */
     void closeEvent(QCloseEvent *event) override;
 
 };
